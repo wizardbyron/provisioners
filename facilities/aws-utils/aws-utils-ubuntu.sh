ae1ae4e5eae696ba501508d0d813e016463f3b2d
GU Yu <ygu@thoughtworks.com>
2017-09-27 11:49:48 +0800

    Add apt repository to install latest version of Ansible.

--- facilities/aws-utils/aws-utils-ubuntu.sh
+++ facilities/aws-utils/aws-utils-ubuntu.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
-sudo apt update -y
+sudo apt update
 sudo apt dist-upgrade -y
+
+# Add repositories
+sudo apt-add-repository ppa:ansible/ansible
+sudo apt-get update
+
+# Install packages via apt
 sudo apt install -y python-pip ansible awscli
+
+# Install packages via pip
 sudo pip install --upgrade pip
 sudo pip install boto
