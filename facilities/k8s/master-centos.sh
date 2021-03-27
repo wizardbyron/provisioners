d612e53f60d7b251a75cbf5bdadb0181322db0f0
Byron <1400427+wizardbyron@users.noreply.github.com>
2021-03-27 15:37:06 +0800

    Create master-centos.sh

---
+++ facilities/k8s/master-centos.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-\$basearch
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+exclude=kubelet kubeadm kubectl
+EOF
+
+# Set SELinux in permissive mode (effectively disabling it)
+sudo setenforce 0
+sudo sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+sudo yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
+sudo systemctl enable --now kubelet
+
+cat <<EOF | sudo tee /etc/docker/daemon.json
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+
+sudo systemctl enable docker.service
+sudo systemctl daemon-reload
+sudo systemctl restart docker
+
+sudo sh -c "echo 1 > /proc/sys/net/bridge/bridge-nf-call-iptable"
+sudo sh -c "echo 1 > /proc/sys/net/ipv4/ip_forward"
+
+cat <<EOF | sudo tee /etc/sysctl.conf 
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+net.bridge.bridge-nf-call-arptables = 0
+EOF
+
+sudo sysctl -p
+sudo swapoff -a
+
+sudo kubeadm init  \
+--apiserver-advertise-address=0.0.0.0 \
+--service-cidr=10.96.0.0/16 \
+--pod-network-cidr=10.244.0.0/16\
+# --image-repository registry.aliyuncs.com/google_containers
+
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+export KUBECONFIG=/etc/kubernetes/admin.conf
