3a0ba7ccb153fad460d9f8266649e1ab04c004f9
Byron <1400427+wizardbyron@users.noreply.github.com>
2021-03-27 14:39:00 +0800

    Update and rename docker-ce.sh to docker-centos.sh

--- facilities/docker-ce/docker-centos.sh
+++ facilities/docker-ce/docker-centos.sh
@@ -2,4 +2,6 @@
 sudo yum install -y wget git
 curl -fsSL https://get.docker.com |sudo sh
 sudo usermod -aG docker vagrant
-sudo service docker start
+sudo systemctl enable docker
+sudo systemctl daemon-reload
+sudo systemctl restart docker
