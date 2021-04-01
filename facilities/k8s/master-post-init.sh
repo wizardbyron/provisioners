05b24ac1521428c5696ee2199c2d548b6135240a
Byron <1400427+wizardbyron@users.noreply.github.com>
2021-04-01 18:49:20 +0800

    Update master-post-init.sh

--- facilities/k8s/master-post-init.sh
+++ facilities/k8s/master-post-init.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+echo "sudo swapoff -a">>$HOME/.bashrc
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
