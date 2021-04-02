5f06d929f58c48d1e493625df245afc680caf69b
Byron <1400427+wizardbyron@users.noreply.github.com>
2021-04-02 15:33:25 +0800

    Update master-centos.sh

--- facilities/k8s/master-centos.sh
+++ facilities/k8s/master-centos.sh
@@ -15,8 +15,8 @@ cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes-aliyun.repo
 name=Kubernetes
 baseurl=https://mirrors.aliyun.com/kubernetes/yum/repos/kubernetes-el7-x86_64/
 enabled=1
-gpgcheck=0
-repo_gpgcheck=0
+gpgcheck=1
+repo_gpgcheck=1
 gpgkey=https://mirrors.aliyun.com/kubernetes/yum/doc/yum-key.gpg https://mirrors.aliyun.com/kubernetes/yum/doc/rpm-package-key.gpg
 exclude=kube*
 EOF
