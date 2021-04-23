9bcbdafc58995b55eadba617105de3dfb84da137
Byron <1400427+wizardbyron@users.noreply.github.com>
2021-04-23 18:13:31 +0800

    Update docker-centos.sh

--- facilities/docker-ce/docker-centos.sh
+++ facilities/docker-ce/docker-centos.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 sudo yum remove docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-engine
-# sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-sudo yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
-sudo yum install -y docker-ce docker-ce-cli containerd.io
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+# sudo sed -i 's+download.docker.com+mirrors.tuna.tsinghua.edu.cn/docker-ce+' /etc/yum.repos.d/docker-ce.repo
+sudo yum install -y docker-ce docker-ce-cli containerd.io makecache fast
 sudo systemctl start docker
 sudo usermod -aG docker vagrant
-pip3 install --user docker-compose -i https://pypi.tuna.tsinghua.edu.cn/simple
+pip3 install --user docker-compose
+# su - vagrant -c "pip3 install --user docker-compose -i https://pypi.tuna.tsinghua.edu.cn/simple"
