50270d03a43a4e69b2ba3a00c80f929606db71bb
wizardbyron <1400427+wizardbyron@users.noreply.github.com>
2021-04-24 11:29:22 +0800

    remove master-post-init.sh

--- facilities/k8s/master-post-init.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-echo "sudo swapoff -a">>$HOME/.bashrc
-mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
-
-# Install Flannel
-kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
