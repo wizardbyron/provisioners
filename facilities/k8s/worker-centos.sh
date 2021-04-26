67840f1ac55916c9bbc10737a8ae13d023d10888
Byron <1400427+wizardbyron@users.noreply.github.com>
2021-04-26 14:50:42 +0800

    Update worker-centos.sh

--- facilities/k8s/worker-centos.sh
+++ facilities/k8s/worker-centos.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
+echo "Setting up firewalld, refer to https://kubernetes.io/docs/setup/production-environment/tools/kubeadm/install-kubeadm/"
+sudo systemctl enable firewalld
+sudo systemctl start firewalld
+sudo firewall-cmd --permanent --add-port=10250/tcp
+sudo firewall-cmd --permanent --add-port=30000-32767/tcp
+sudo firewall-cmd --reload
+
 echo "Getting join-cluster.sh from $1"
 curl -fsSL http://$1:8000/join-cluster.sh | sudo bash
