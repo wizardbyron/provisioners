d51b418715dcc52d7769457dfd0e28efc25734c4
wizardbyron <1400427+wizardbyron@users.noreply.github.com>
2021-08-03 07:16:11 +0800

    Update jenkins provision script for centos

--- facilities/jenkins/jenkins-centos.sh
+++ facilities/jenkins/jenkins-centos.sh
@@ -3,5 +3,4 @@ sudo yum install -y java-11-openjdk
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 sudo yum install -y jenkins
-sudo usermod -aG docker vagrant
 sudo service jenkins start
