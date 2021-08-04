a24307ad5090150538ca38b200f0ed1acf03d8db
wizardbyron <1400427+wizardbyron@users.noreply.github.com>
2021-08-04 22:39:27 +0800

    Move firewalld installation in ubuntu provision script.

--- facilities/k8s/installation-ubuntu.sh
+++ facilities/k8s/installation-ubuntu.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 sudo apt-get update
-sudo apt-get install -y apt-transport-https ca-certificates curl firewalld
+sudo apt-get install -y apt-transport-https ca-certificates curl
 sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
