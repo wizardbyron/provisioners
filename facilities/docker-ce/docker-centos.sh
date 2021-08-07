c9746c0b0802051afad78e1798957e5f9c6a98cc
wizardbyron <1400427+wizardbyron@users.noreply.github.com>
2021-08-07 15:26:04 +0800

    Move docker-ce to essential provision script

--- facilities/docker-ce/docker-centos.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-sudo yum remove docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-engine
-sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-sudo yum install -y docker-ce docker-ce-cli containerd.io makecache fast
-sudo systemctl start docker
-sudo usermod -aG docker vagrant
-pip3 install --user docker-compose
--- facilities/docker-ce/docker.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-echo "Install docker-ce via official installation script."
-curl -fsSL https://get.docker.com | sudo bash
-
-if [ $? = 0 ]; then
-sudo usermod -aG docker $(whoami)
-sudo systemctl restart docker
-python3 -m pip install --user docker-compose
-fi
