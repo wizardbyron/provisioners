eccb39d0fa80d7232f2a0853f9d0b4183a3d1755
wizardbyron <1400427+wizardbyron@users.noreply.github.com>
2021-08-07 16:44:29 +0800

    Add kubernetes installation script

---
+++ solutions/k8s/install.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+
+echo "Install Kubernetes packages via package manager."
+if [ -n "$(command -v yum)" ];then # for centos
+cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-\$basearch
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+exclude=kubelet kubeadm kubectl
+EOF
+
+# Set SELinux in permissive mode (effectively disabling it)
+sudo setenforce 0
+sudo sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+sudo yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
+
+elif [ -n "$(command -v apt)" ];then # ubuntu
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl
+sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+else
+echo "Your Linux package manager hasn't support"
+exit 1
+fi
+
+if [ $? = 0 ]; then
+sudo systemctl enable --now kubelet
+
+# Update docker settings
+cat <<EOF | sudo tee /etc/docker/daemon.json
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+
+sudo systemctl enable docker.service
+sudo systemctl daemon-reload
+sudo systemctl restart docker
+
+# Setup Network and firewall
+cat <<EOF | sudo tee /etc/sysctl.conf 
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+net.bridge.bridge-nf-call-arptables = 0
+EOF
+sudo sysctl -p
+
+# Switch off swap
+sudo swapoff -a
+
+echo "sudo swapoff -a">>$HOME/.bashrc
+fi
