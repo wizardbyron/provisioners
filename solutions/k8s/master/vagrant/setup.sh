ba83b0f5f73c6e0346da217ed4e21d48ecce3d9a
wizardbyron <1400427+wizardbyron@users.noreply.github.com>
2021-08-07 23:43:31 +0800

    Update Kubernetes cluster and worker node provision scripts.

---
+++ solutions/k8s/master/vagrant/setup.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+PATH=$PATH:/home/$(whoami)/.local/bin
+rm -rf ./provisioners-master master.zip
+wget https://github.com/wizardbyron/provisioners/archive/refs/heads/master.zip
+unzip master.zip
+
+sh -c "./provisioners-master/facilities/k8s/install.sh"
+sh -c "./provisioners-master/facilities/k8s/setup-cluster.sh"
