50d6b12731029192bdac3b4329e557207affd70f
顾宇（Gu Yu） <1400427+wizardbyron@users.noreply.github.com>
2021-08-09 11:09:09 +0800

    Update setup.sh

--- solutions/devops/master/vagrant/setup.sh
+++ solutions/devops/master/vagrant/setup.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 PATH=$PATH:/home/$(whoami)/.local/bin
-rm -rf ./provisioners-master master.zip
+sudo rm -rf ./provisioners-master master.zip
 wget https://github.com/wizardbyron/provisioners/archive/refs/heads/master.zip
 unzip master.zip
-sh -c "./provisioners-master/facilities/jenkins.sh"
-sh -c "./provisioners-master/facilities/wekan.sh"
-sh -c "./provisioners-master/facilities/nexus.sh"
-docker-compose -f ./provisioners-master/facilities/gitea/docker-compose.yml up
+sh -c "./provisioners-master/facilities/jenkins/jenkins.sh"
+sh -c "./provisioners-master/facilities/wekan/wekan.sh"
+sh -c "./provisioners-master/facilities/nexus/nexus.sh"
+docker-compose -f ./provisioners-master/facilities/gitea/docker-compose.yml up -d 
