d75e6e6882fc75f9c2c5a5bc41ac5ac4f23c8af8
wizardbyron <1400427+wizardbyron@users.noreply.github.com>
2021-08-09 22:39:05 +0800

    Update k8s cluster and worker setup scripts.

--- solutions/k8s/master/vagrant/setup.sh
+++ solutions/k8s/master/vagrant/setup.sh
@@ -3,6 +3,6 @@ PATH=$PATH:/home/$(whoami)/.local/bin
 rm -rf ./provisioners-master master.zip
 wget https://github.com/wizardbyron/provisioners/archive/refs/heads/master.zip
 unzip master.zip
-
+export CLUSTER_IP=$(cat /home/$(whoami)/CLUSTER_IP)
 sh -c "./provisioners-master/facilities/k8s/install.sh"
-sh -c "./provisioners-master/facilities/k8s/setup-cluster.sh"
+sh -c "./provisioners-master/facilities/k8s/setup-cluster.sh $CLUSTER_IP"
