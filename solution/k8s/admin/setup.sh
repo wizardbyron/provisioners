ff819392392b610b3055dfa0823bff45c2aa54b9
wizardbyron <1400427+wizardbyron@users.noreply.github.com>
2022-02-07 17:12:38 +0800

    rename IMAGE_REPO to MIRROR

--- solution/k8s/admin/setup.sh
+++ solution/k8s/admin/setup.sh
@@ -3,7 +3,8 @@ PATH=$PATH:/home/$(whoami)/.local/bin
 export CONTROL_PANEL_IP=$1
 echo "CONTROL PANEL IP: $CONTROL_PANEL_IP"
 
-IMAGE_REPO="aliyun" # <empty>/aliyun
+MIRROR="aliyun" # <empty>/aliyun
 KUBE_VERSION="1.23.0-0"
-sh -c "/vagrant/facility/k8s/install.sh $KUBE_VERSION $IMAGE_REPO"
-sh -c "/vagrant/facility/k8s/setup-control-panel.sh $CONTROL_PANEL_IP $IMAGE_REPO"
+
+sh -c "/vagrant/facility/k8s/install.sh $KUBE_VERSION $MIRROR"
+sh -c "/vagrant/facility/k8s/setup-control-panel.sh $CONTROL_PANEL_IP $MIRROR"
