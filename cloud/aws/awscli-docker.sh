3028522144e9d3da4941a5067fd7a44bfc9b9eaf
wizardbyron <1400427+wizardbyron@users.noreply.github.com>
2022-11-10 18:21:29 +0800

    remove cloud

--- cloud/aws/awscli-docker.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-sudo docker pull amazon/aws-cli 
-sudo rm -rf $HOME/.aws
-mkdir $HOME/.aws
-alias aws="docker run --rm -it -v $HOME/.aws:/root/.aws -v /vagrant:/vagrant amazon/aws-cli"
